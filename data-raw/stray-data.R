95b403b94b8fe35f0892908276747b0faf5f8b56
liz-fw <lstebbins@flowwest.com>
2026-03-03 11:51:15 -0800

    adds hatchery flows through 2000 for action 5

--- data-raw/stray-data.R
+++ data-raw/stray-data.R
@@ -38,6 +38,20 @@ hatchery_oct_nov_flows$LTO_12a <- flows_cfs$LTO_12a |>
   `row.names<-`(1980:2002) |>
   t()
 
+hatchery_oct_nov_flows$action_5 <- flows_cfs$action_5 |>
+  select(date, tidyselect::all_of(as.character(hatchery_to_watershed_lookup))) |>
+  filter(month(date) %in% 10:11) |>
+  pivot_longer(-date, names_to = "watershed", values_to = "flow_cfs") |>
+  group_by(year = year(date), watershed) |>
+  summarise(avg_flow_cfs = mean(flow_cfs)) |>
+  ungroup() |>
+  filter(year %in% 1980:2002) |>
+  pivot_wider(names_from = "watershed", values_from = "avg_flow_cfs") |>
+  select(-year) |>
+  as.matrix() |>
+  `row.names<-`(1980:2000) |>
+  t()
+
 usethis::use_data(hatchery_oct_nov_flows, overwrite = TRUE)
 
 
@@ -69,6 +83,20 @@ hatchery_apr_may_flows$LTO_12a <- flows_cfs$LTO_12a |>
   select(-year) |>
   as.matrix() |>
   `row.names<-`(1980:2002) |>
+  t()
+
+hatchery_apr_may_flows$action_5 <- flows_cfs$action_5 |>
+  select(date, tidyselect::all_of(as.character(hatchery_to_watershed_lookup))) |>
+  filter(month(date) %in% 4:5) |>
+  pivot_longer(-date, names_to = "watershed", values_to = "flow_cfs") |>
+  group_by(year = year(date), watershed) |>
+  summarise(avg_flow_cfs = mean(flow_cfs)) |>
+  ungroup() |>
+  filter(year %in% 1980:2002) |>
+  pivot_wider(names_from = "watershed", values_from = "avg_flow_cfs") |>
+  select(-year) |>
+  as.matrix() |>
+  `row.names<-`(1980:2000) |>
   t()
 
 usethis::use_data(hatchery_apr_may_flows, overwrite = TRUE)
